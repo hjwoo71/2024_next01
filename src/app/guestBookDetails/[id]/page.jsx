7fa079b783c41627448d2bcfcbba25a438bb9a77
brixon <brixon@naver.com>
2024-12-05 12:23:04 +0900

    guestbook 완성본

--- src/app/guestBookDetails/[id]/page.jsx
+++ src/app/guestBookDetails/[id]/page.jsx
@@ -1,98 +1,169 @@
-"use client"
-import axios from "axios";
+"use client";
+
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material';
+import axios from 'axios';
 import { useEffect, useState } from "react";
 import './guestBookDetails.css'
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import useAuthStore from '../../../../store/authStore';
+import { useRouter } from 'next/navigation';
 
-function Page({params}) {
-  const BASE_API_BASE_URL = process.env.NEXT_PUBLIC_LOCAL_API_BASE_URL;
-  const [item, setItem] = useState(null); // 데이터 상태
-  const [loading, setLoading] = useState(true); // 로딩 상태
-  const [error, setError] = useState(null); // 에러 상태
 
-  useEffect(() => {
-      const fetchData = async () => {
-          try {
-              setLoading(true); // 로딩 시작
-              // params 언래핑: Promise로 감싸진 값을 꺼내는 과정
-              // Promise.resolve(params)의 역할
-              // Promise.resolve()는 전달된 값을 Promise 객체로 변환합니다.
-              // 만약 params가 이미 Promise라면, 원래 Promise를 반환합니다.
-              // 만약 params가 일반 객체라면, 이를 즉시 해결된(resolved) Promise로 감쌉니다.
-              // Promise인지 아닌지 신경 쓰지 않고 항상 비동기적으로 다룰 수 있습니다.
-              // const resolvedParams = await Promise.resolve(params); // params 언래핑
-              // const { id } = resolvedParams; // id 추출
-              const { id } = await Promise.resolve(params);
-              const API_URL = `${BASE_API_BASE_URL}/guestbook/detail/${id}`;
+function Page({ params }) {
+    const LOCAL_API_BASE_URL = process.env.NEXT_PUBLIC_LOCAL_API_BASE_URL;
+    const LOCAL_IMG_URL = process.env.NEXT_PUBLIC_LOCAL_IMG_URL;
+    const [item, setItem] = useState(null);       // 데이터 상태
+    const [loading, setLoading] = useState(true); // 로딩 상태
+    const [error, setError] = useState(null);     // 에러 상태
+    const { isAuthenticated, token } = useAuthStore();       // 로그인 상태
+    const router = useRouter();
 
-              // 데이터 가져오기
-              const response = await axios.get(API_URL);
-              const data = response.data;
-              if(data.success){
-                   setItem(data.data);
-              }else{
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setLoading(true); // 로딩 시작
+
+                const { id } = await Promise.resolve(params);
+                const API_URL = `${LOCAL_API_BASE_URL}/guestbook/detail/${id}`;
+
+                // 데이터 가져오기
+                const response = await axios.get(API_URL);
+                const data = response.data;
+                if (data.success) {
+                    setItem(data.data);
+                } else {
                     setError("Failed to fetch product data.");
-              }
+                }
+            } catch (err) {
+                console.error("Error fetching product data:", err);
+                setError("Failed to fetch product data.");
+            } finally {
+                setLoading(false); // 로딩 종료
+            }
+        };
 
-          } catch (err) {
-              console.error("Error fetching product data:", err);
-              setError("Failed to fetch product data.");
-          } finally {
-              setLoading(false); // 로딩 종료
-          }
-      };
+        fetchData();
+    }, [params, LOCAL_API_BASE_URL]);
 
-      fetchData();
-  }, [params, BASE_API_BASE_URL]);
+    // delete
+    const handleDelete = async () => {
+        // 버트를 항상 활성화 하면 
+        // if (!isAuthenticated) {
+        //     alert("로그인이 필요합니다.")
+        //     router.push("/login");
+        // }
 
-  // 로딩 중
-  if (loading) {
-      return <div style={{ textAlign: "center", padding: "20px" }}>Loading...</div>;
-  }
+        // 상세보기 성공했을 때 데이터 item에 넣었다.
+        const API_URL = `${LOCAL_API_BASE_URL}/guestbook/delete/${item.gb_idx}`;
+        try {
+            const response = await axios.get(API_URL, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            if (response.data.success) {
+                alert(response.data.message);
+                router.push("/guestBookList")
+            } else {
+                alert(response.data.message);
+            }
+        } catch (error) {
+            console.error("delete error");
+        }
+    }
 
-  // 에러 발생 시
-  if (error) {
-      return (
-          <div style={{ textAlign: "center", padding: "20px", color: "red" }}>
-              <h2>Error:</h2>
-              <p>{error}</p>
-          </div>
-      );
-  }
-  return (
-    <>
-     <h2 className="title">GuestBookList</h2>
+    // update
+    const handleUpdate = async () => {
+        // 수정페이지로 이동
+        router.push(`/guestBookUpdate/${item.gb_idx}`)
+    }
+
+    // 로딩 중
+    if (loading) {
+        return <div style={{ textAlign: "center", padding: "20px" }}>Loading...</div>;
+    }
+
+    // 에러 발생 시
+    if (error) {
+        return (
+            <div style={{ textAlign: "center", padding: "20px", color: "red" }}>
+                <h2>Error:</h2>
+                <p>{error}</p>
+            </div>
+        );
+    }
+
+    // 로딩 완료 후
+    return (
+        <>
+            <h2 className="title">GuestBookList</h2>
             <TableContainer component={Paper} className="table-container">
                 <Table className="custom-table">
                     <TableBody>
+                        <TableRow>
+                            <TableCell className="table-cell">NAME</TableCell>
+                            <TableCell className="table-cell">{item.gb_name}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="table-cell">SUBJECT</TableCell>
+                            <TableCell className="table-cell">{item.gb_subject}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="table-cell">CONTENT</TableCell>
+                            <TableCell className="table-cell"><pre>{item.gb_content}</pre></TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="table-cell">EMAIL</TableCell>
+                            <TableCell className="table-cell">{item.gb_email}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="table-cell">DATE</TableCell>
+                            <TableCell className="table-cell">{item.gb_regdate.substring(0, 10)}</TableCell>
+                        </TableRow>
+                        {item.gb_filename && (
                             <TableRow>
-                                <TableCell className="table-cell">IDX</TableCell>
-                                <TableCell className="table-cell">{item.gb_idx}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell className="table-cell">NAME</TableCell>    
-                                <TableCell className="table-cell">{item.gb_name}</TableCell>
-                             </TableRow>
-                             <TableRow>   
-                                <TableCell className="table-cell">SUBJECT</TableCell>
-                                <TableCell className="table-cell">{item.gb_subject}</TableCell>
-                              </TableRow>
-                              <TableRow>
-                                <TableCell className="table-cell">CONTENT</TableCell> 
-                                <TableCell className="table-cell">{item.gb_content}</TableCell>
-                              </TableRow>
-                              <TableRow>
-                                <TableCell className="table-cell">EMAIL</TableCell>
-                                <TableCell className="table-cell">{item.gb_email}</TableCell>
-                              </TableRow>
-                              <TableRow>
-                                 <TableCell className="table-cell">DATE</TableCell>
-                                <TableCell className="table-cell">{item.gb_regdate}</TableCell>
-                            </TableRow>
+                            <TableCell className="table-cell">Image</TableCell>
+                            <TableCell className="table-cell">
+                              {isAuthenticated ? (
+                                <a
+                                  href={`${LOCAL_API_BASE_URL}/guestbook/download/${item.gb_filename}`} // Spring Boot 다운로드 링크
+                                  download={item.gb_filename} // 다운로드 파일 이름 지정
+                                  target="_blank" // 새 탭에서 열림
+                                  rel="noopener noreferrer" // 보안 향상을 위해 추가
+                                >
+                                  <img
+                                    src={`${LOCAL_IMG_URL}/${item.gb_filename}`}
+                                    alt="Uploaded Image"
+                                    style={{ width: "150px" }}
+                                  />
+                                </a>
+                              ) : (
+                                <img
+                                  src={`${LOCAL_IMG_URL}/${item.gb_filename}`}
+                                  alt="Uploaded Image"
+                                  style={{ width: "150px" }}
+                                />
+                              )}
+                            </TableCell>
+                          </TableRow>
+                        )}
                     </TableBody>
                 </Table>
-            </TableContainer> 
-    </>
-  );
+            </TableContainer>
+            <div style={{ margin: "20px", textAlign: "center" }}>
+                <Button variant='contained'
+                    color='primary'
+                    onClick={handleUpdate}
+                    disabled={!isAuthenticated}
+                >수정</Button>
+
+                <Button variant='contained'
+                    color='error'
+                    onClick={handleDelete}
+                    style={{ marginLeft: "10px" }}
+                    disabled={!isAuthenticated}
+                >삭제</Button>
+            </div>
+        </>
+    );
 }
 export default Page;
